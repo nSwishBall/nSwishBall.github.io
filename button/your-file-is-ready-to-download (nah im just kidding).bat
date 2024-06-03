f6b19ce2aaec13752e22e15db6a7d419aacf03cf
nSwishBall <157294157+TOPBYT-GitHub@users.noreply.github.com>
2024-06-03 19:28:55 +0200

    as
    sa

---
+++ button/your-file-is-ready-to-download (nah im just kidding).bat
@@ -0,0 +1,57 @@
+@echo off
+echo Downloading file... (2/10)
+echo.
+echo --------------------------------------------------
+timeout /t 0 /nobreak > nul
+cls
+echo Downloading file... (1/10)
+echo.
+echo oo------------------------------------------------
+timeout /t 0 /nobreak > nul
+cls
+echo Downloading file... (2/10)
+echo.
+echo oooo----------------------------------------------
+timeout /t 0 /nobreak > nul
+cls
+echo Downloading file... (3/10)
+echo.
+echo oooooo--------------------------------------------
+timeout /t 0 /nobreak > nul
+cls
+echo Downloading file... (4/10)
+echo.
+echo ooooooooo-----------------------------------------
+timeout /t 0 /nobreak > nul
+cls
+echo Downloading file... (5/10)
+echo.
+echo oooooooooo----------------------------------------
+timeout /t 0 /nobreak > nul
+cls
+echo Downloading file... (6/10)
+echo.
+echo ooooooooooooo-------------------------------------
+timeout /t 0 /nobreak > nul
+cls
+echo Downloading file... (7/10)
+echo.
+echo ooooooooooooooooo---------------------------------
+timeout /t 0 /nobreak > nul
+cls
+echo Downloading file... (8/10)
+echo.
+echo ooooooooooooooooooooooooo-------------------------
+timeout /t 0 /nobreak > nul
+cls
+echo Downloading file... (9/10)
+echo.
+echo oooooooooooooooooooooooooooooooooooo--------------
+timeout /t 1 nobreak > nul
+cls
+echo Downloading file... (10/10)
+echo.
+echo oooooooooooooooooooooooooooooooooooooooooooooooooo
+timeout /t 0 /nobreak > nul
+cls
+md *pop* woahh...
